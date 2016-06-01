a6ddaa9f0ca87e17bd63da92156a531b60d4906a
Sophia K <Sophe@Phia.local>
2016-06-01 14:14:11 +0200

    Tests

--- modules/detectSmells/__init__.py
+++ modules/detectSmells/__init__.py
@@ -19,14 +19,14 @@ def update_file(context, f):
             #----
             #get required paths
             dir = os.path.dirname(__file__)
-            subdir = "101worker" + os.sep + "modules" + os.sep + "detectSmells"
-            if not dir.endswith(subdir):
-                print("Error: Path is false!")
+            #subdir = "101worker" + os.sep + "modules" + os.sep + "detectSmells"
+            #if not dir.endswith(subdir):
+            #    print("Error: Path is false!")
                 
             #path concatination
-            rootDir=dir[:-len(subdir)]    
-            dataPath = rootDir + "101results" + os.sep + "101repo" + os.sep + f
-            
+            #rootDir=dir[:-len(subdir)]
+            #dataPath = rootDir + "101results" + os.sep + "101repo" + os.sep + f
+            dataPath = os.path.join(context.get_env("repo101dir"), f)
 			#check smells for source-files
             command = "java -jar " + dir + os.sep + "checkstyle.jar -c " + dir + os.sep + "checkstyle_checks_sun.xml " + dataPath + " -f xml"
             cmdResult = subprocess.check_output(command, shell=True)
@@ -41,7 +41,7 @@ def update_file(context, f):
         
             context.write_derived_resource(f, jsonText, 'smell')
     except UnicodeDecodeError:
-        context.write_derived_resource(f, 0, 'smell')
+        context.write_derived_resource(f, "", 'smell')
 
 def remove_file(context, f):
     context.remove_derived_resource(f, 'smell')
@@ -56,3 +56,39 @@ def run(context, change):
 
     else:
         remove_file(context, change['file'])
+
+
+import unittest
+from unittest.mock import Mock
+
+class DetectSmellsTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.env = Mock()
+        #pfad zum richtigen file setzen
+        self.env.get_primary_resource.return_value = 'x = 5\ny=6\nprint(x)\n'
+    
+    def test_run(self):
+        change = {
+            'type': 'NEW_FILE',
+            'file': 'some-file.java'
+        }
+        run(self.env, change)
+        #assert string mit samplefile
+    self.env.write_derived_resource.assert_called_with(dir + 'some-file.py', 4, 'loc')
+
+
+def test_run_removed(self):
+    change = {
+        'type': '',
+            'file': 'some-file.java'
+        }
+        run(self.env, change)
+        
+    self.env.remove_derived_resource.assert_called_with('some-file.java', 'smell')
+
+
+def test():
+    suite = unittest.TestLoader().loadTestsFromTestCase(DetectSmellsTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
